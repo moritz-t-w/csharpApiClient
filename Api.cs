8cb1205c32e2873d107f74beb3c720d4028720d6
Moritz T. W <90388353+moritz-t-w@users.noreply.github.com>
2023-03-28 19:11:09 +0200

    Create Api class

---
+++ Api.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Api
+{
+    public class Api
+    {
+        /** Base URL with a parameter for the version */
+        private readonly Uri _baseUrl;
+        /** Endpoints */
+        public readonly List<Endpoint> Endpoints;
+
+        public Api(Uri baseUrl, List<Endpoint> endpoints)
+        {
+            _baseUrl = baseUrl ?? throw new ArgumentNullException(nameof(baseUrl));
+            Endpoints = endpoints ?? throw new ArgumentNullException(nameof(endpoints));
+        }
+    }
+}
