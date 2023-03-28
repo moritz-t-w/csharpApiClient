b82693b86a1a44e5edc64893f522135485b83530
Moritz T. W <90388353+moritz-t-w@users.noreply.github.com>
2023-03-28 23:12:12 +0200

    all readonly

--- Parameter.cs
+++ Parameter.cs
@@ -2,7 +2,7 @@ namespace Api
 {
 	public class Parameter<T> where T : Type
 	{
-		public bool Required = false;
-		public T? DefaultValue;
+		public readonly bool Required = false;
+		public readonly T? DefaultValue;
 	}
 }
