d4e45c7b9c563db15cce1ef4d1649f059be9248b
Moritz T. W <90388353+moritz-t-w@users.noreply.github.com>
2023-03-28 19:51:15 +0200

    That'll do

---
+++ Client.cs
@@ -0,0 +1,14 @@
+﻿namespace Api
+{
+    public class Client
+    {
+        private readonly Api _api;
+        private readonly HttpClient _client;
+        private readonly IAuthenticator _authenticator;
+        public Client(Api api, HttpClient client, IAuthenticator authenticator)
+        {
+            _api = api ?? throw new ArgumentNullException(nameof(api));
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+            _authenticator = authenticator ?? throw new ArgumentNullException(nameof(authenticator));
+        }
+    }
