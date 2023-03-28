c72dd9137e8df608e7a89afff1be36389da37912
Moritz T. W <90388353+moritz-t-w@users.noreply.github.com>
2023-03-28 20:40:31 +0200

    Add Authentication method

--- Api.cs
+++ Api.cs
@@ -2,17 +2,20 @@
 
 namespace Api
 {
-    public class Api
+    public class Api<TAuthentication> where TAuthentication : IAuthenticator
     {
         /** Base URL */
         private readonly Uri _baseUrl;
         /** Endpoints */
         public readonly List<Endpoint> Endpoints;
+        /** Authentication */
+        public readonly Type Authentication;
 
         public Api(Uri baseUrl, List<Endpoint> endpoints)
         {
             _baseUrl = baseUrl ?? throw new ArgumentNullException(nameof(baseUrl));
             Endpoints = endpoints ?? throw new ArgumentNullException(nameof(endpoints));
+            this.Authentication = typeof(TAuthentication);
         }
     }
 }
