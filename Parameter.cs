5aa16abdda49faac6af4c50c6c9aa428c51458d3
Moritz T. W <90388353+moritz-t-w@users.noreply.github.com>
2023-03-28 23:13:11 +0200

    add the type as prop so it's accessible

--- Parameter.cs
+++ Parameter.cs
@@ -4,5 +4,13 @@ namespace Api
 	{
 		public readonly bool Required = false;
 		public readonly T? DefaultValue;
+		public readonly Type Type;
+
+		public Parameter(bool required, T? defaultValue)
+		{
+			Required = required;
+			DefaultValue = defaultValue;
+			Type = typeof(T);
+		}
 	}
 }
