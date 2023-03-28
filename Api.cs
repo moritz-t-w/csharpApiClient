cb0f96d43b5b4800f95490a656881764c55ad3b0
Moritz T. W <90388353+moritz-t-w@users.noreply.github.com>
2023-03-29 00:47:31 +0200

    suddenly, this works
    huh, what a coincidence

--- Api.cs
+++ Api.cs
@@ -7,11 +7,11 @@ namespace Api
 		/** Base URL */
 		internal readonly Uri BaseUrl;
 		/** Endpoints */
-		public readonly List<Endpoint> Endpoints;
+		public readonly List<Endpoint<object>> Endpoints;
 		/** Authentication Methods */
 		public readonly List<Type> AuthenticationMethods;
 
-		public Api(Uri baseUrl, List<Endpoint> endpoints, List<Type> authenticationMethods)
+		public Api(Uri baseUrl, List<Endpoint<object>> endpoints, List<Type> authenticationMethods)
 		{
 			BaseUrl = baseUrl;
 			Endpoints = endpoints;
